96ef2c14fb1733d405f30510090f7504a4482457
청 <riplac0314@naver.com>
2020-07-20 21:10:17 +0900

    aaa

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
 import TOC from "./components/TOC";
-import Content from "./components/Content";
+import ReadContent from "./components/ReadContent";
+import CreateContent from "./components/CreateContent";
 import Subject from "./components/Subject";
+import Control from "./components/Control";
 
 
 class App extends Component{
   constructor(props){
     super(props);
     this.state ={
-      mode:'read',
-      selected_content_id:2,
+      mode:'create',
+      selected_content_id:1,
       subject:{title:"WEB", sub:"World Wide Web!"},
       welcome:{title:'Welcome',desc:'Hello React!!'},
       contents:[
@@ -20,35 +22,56 @@ class App extends Component{
       ]
     }
   }
+
   render(){
-    var _title, _desc = null;
+    var _title, _desc, _article=null;
     if(this.state.mode === 'welcome'){
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
-    }else if(this.state.mode === 'read'){
+      _article = <ReadContent title={_title} desc={_desc}></ReadContent>;
+    }
+
+    else if(this.state.mode === 'read'){
       let i = 0;
       while(i < this.state.contents.length){
         var data = this.state.contents[i];
         if(data.id === this.state.selected_content_id){
           _title = data.title;
           _desc = data.desc;
-          break
+          break;
         }
         i = i + 1;
       }
-      _title = this.state.contents[0].title;
-      _desc = this.state.contents[0].desc;
+      _article = <ReadContent title={_title} desc={_desc}></ReadContent>;
     }
+    else if(this.state.mode === "create"){
+      _article = <CreateContent onSubmit={function(_title,_desc){
+        //add content to this.state.contents
+      }.bind(this)}></CreateContent>;
+    }
+
     return(
       <div className="App">
+
       <Subject title= {this.state.subject.title} sub={this.state.subject.sub} onChangePage= {function(){
         this.setState({mode:"welcome"});
       }.bind(this)} ></Subject>
 
-      <TOC onChangePage={function(){
-        this.setState({mode:'read'});
+      <TOC onChangePage={function(id){
+        this.setState({
+          mode:'read',
+          selected_content_id:Number(id)
+        });
       }.bind(this)}data={this.state.contents}></TOC>
-      <Content title={_title} desc={_desc}></Content>
+
+      <Control onChangeMode={function(_mode){
+        this.setState({
+          mode:_mode
+        })
+      }.bind(this)}></Control>
+
+      {_article}
+      
     </div>
     );
   }
---
+++ src/components/CreateContent.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class CreateContent extends Component{
+  render(){
+    return(
+      <article>
+        <h2>Create</h2>
+        <form action = "/create_process" method="post" onSubmit={function(e){
+          e.preventDefault();
+        }.bind(this)}>
+          <p><input type = "text" name="title" placeholder="title"></input></p>
+          <p><textarea name="desc" placeholder="description"></textarea></p>
+          <p><input type="submit"></input></p>
+        </form>
+      </article>
+    );
+  }
+}
+
+export default CreateContent;
